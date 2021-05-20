dc6781a8df1e925f8c402f3d1ce50dd3a4c468a5
Chemllei Joseph <chemllei0304@gmail.com>
2021-05-19 20:32:22 -0400

    Make all folders and pages a global variable

--- modules/views/__init__.py
+++ modules/views/__init__.py
@@ -7,6 +7,6 @@ from modules.model import Folder, Page
 
 @app.context_processor
 def inject_recent():
-    recent_folders = DB().read_all(Folder).order_by(text("date_created desc"))[:5]
-    recent_pages = DB().read_all(Page).order_by(text("last_modified desc"))[:5]
-    return dict(recent_folders=recent_folders, recent_pages=recent_pages)
+    folders_ = DB().read_all(Folder).order_by(text("date_created desc"))
+    pages_ = DB().read_all(Page).order_by(text("last_modified desc"))
+    return dict(folders_=folders_, pages_=pages_)

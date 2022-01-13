dc1dd96259e2842d22d1f93aaf7d5ba225c97341
Chemllei Joseph <chemllei0304@gmail.com>
2022-01-12 20:08:16 -0500

    Update config

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ import dotenv
 dotenv.load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-SQLALCHEMY_DATABASE_URI = os.getenv("CLEARDB_DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.getenv("db_url")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': 60}
-DEBUG = True
+DEBUG = os.getenv("debug")
+ENV = os.getenv("env")
